6402273522ba2ca92e8d057d52e6bc95139813ba
ShakChunni <f.m.ashfaq@g.bracu.ac.bd>
2023-10-01 00:39:39 +0600

    connected db

--- dbConnection.js
+++ dbConnection.js
@@ -1,26 +1,57 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-const dbConnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'clinique-management',
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "clinique-management",
 });
 
-function connectToDatabase() {
-  return new Promise((resolve, reject) => {
-    dbConnection.connect((err) => {
-      if (err) {
-        console.error('Error connecting to the database:', err);
-        reject(err);
-      } else {
-        console.log('Connected to the database');
-        resolve(dbConnection);
-      }
-    });
-  });
-}
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+  } else {
+    console.log("Connected to MySQL database");
+  }
+});
 
-module.exports = {
-  connectToDatabase,
-};
+const express = require("express");
+const port = 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static("operators"));
+
+app.get("/clinique-accounts", (req, res) => {
+  res.sendFile(__dirname + "/operators/add-patient.html");
+});
+
+app.post("/formPost", (req, res) => {
+  console.log("Received a POST request to /formPost");
+
+  // Extract data from the request body
+  const { name, age, contact, date } = req.body;
+
+  // Insert data into the MySQL database
+  const sql =
+    "INSERT INTO patient (name, age, contact, date) VALUES (?, ?, ?, ?)";
+  const values = [name, age, contact, date];
+
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      console.error("Error inserting data into the database:", err);
+      res
+        .status(500)
+        .json({ error: "An error occurred while adding the patient." });
+    } else {
+      console.log("Data inserted into the database.");
+      res.status(200).json({ message: "Patient added successfully." });
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
