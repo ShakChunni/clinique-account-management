e57a55e718685b05ec7ce3b5fdf2a15e77aed0e5
ShakChunni <f.m.ashfaq@g.bracu.ac.bd>
2023-10-02 03:15:55 +0600

    working on existing patient

--- dbConnection.js
+++ dbConnection.js
@@ -1,4 +1,7 @@
 const mysql = require("mysql");
+const express = require("express");
+const app = express();
+const port = 5000;
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -15,39 +18,51 @@ db.connect((err) => {
   }
 });
 
-const express = require("express");
-const port = 5000;
-
-const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static("operators"));
 
+// Serve the HTML page for adding patients
 app.get("/clinique-accounts", (req, res) => {
   res.sendFile(__dirname + "/operators/add-patient.html");
 });
 
+// Endpoint to add a new patient to the database
 app.post("/formPost", (req, res) => {
   console.log("Received a POST request to /formPost");
-
-  // Extract data from the request body
   const { name, age, contact, date } = req.body;
-
-  // Insert data into the MySQL database
-  const sql =
-    "INSERT INTO patient (name, age, contact, date) VALUES (?, ?, ?, ?)";
+  const sql = "INSERT INTO patient (name, age, contact, date) VALUES (?, ?, ?, ?)";
   const values = [name, age, contact, date];
 
   db.query(sql, values, (err, result) => {
     if (err) {
       console.error("Error inserting data into the database:", err);
-      res
-        .status(500)
-        .json({ error: "An error occurred while adding the patient." });
+      res.status(500).json({ error: "An error occurred while adding the patient." });
     } else {
       console.log("Data inserted into the database.");
       res.status(200).json({ message: "Patient added successfully." });
+    }
+  });
+});
+
+// Endpoint to retrieve patient information by ID
+app.get("/getPatient/:id", (req, res) => {
+  const patientId = req.params.id;
+  const sql = "SELECT * FROM patient WHERE id = ?";
+  
+  db.query(sql, [patientId], (err, result) => {
+    if (err) {
+      console.error("Error retrieving patient data from the database:", err);
+      res.status(500).json({ error: "An error occurred while fetching patient data." });
+    } else {
+      if (result.length === 0) {
+        // Patient not found
+        res.status(404).json({ message: "Patient not found." });
+      } else {
+        // Patient found, send the data
+        const patientData = result[0];
+        res.status(200).json(patientData);
+      }
     }
   });
 });
