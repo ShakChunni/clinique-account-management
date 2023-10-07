f5f06db5970f8357467be134db40ef271d4eb8ca
ShakChunni <f.m.ashfaq@g.bracu.ac.bd>
2023-10-07 21:16:28 +0600

    operator part kindda done

--- dbConnection.js
+++ dbConnection.js
@@ -2,6 +2,7 @@ const mysql = require("mysql");
 const express = require("express");
 const app = express();
 const port = 5000;
+const multer = require("multer");
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -21,6 +22,21 @@ db.connect((err) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("operators"));
+
+// Configure multer for handling file uploads
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "uploads/"); // Specify the destination directory for uploaded files
+  },
+  filename: (req, file, callback) => {
+    callback(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+// Add multer middleware for handling file uploads
+app.use(upload.single("image")); // 'image' should match the input field name in the form
 
 // Serve the HTML page for adding patients
 app.get("/clinique-accounts", (req, res) => {
@@ -94,6 +110,37 @@ app.post("/update-patient/:id", (req, res) => {
     } else {
       console.log("Patient data updated.");
       res.status(200).json({ message: "Patient data updated successfully." });
+    }
+  });
+});
+
+// Serve the HTML page for adding expenditures
+app.get("/add-expenditure", (req, res) => {
+  res.sendFile(__dirname + "/operators/expenditure.html");
+});
+
+// Endpoint to add a new expenditure to the database
+app.post("/add-expenditure", (req, res) => {
+  console.log("Received a POST request to /add-expenditure");
+
+  // Handle form data, including file uploads
+  const { description, cost } = req.body;
+  const imageName = req.file.filename; // Assuming you're using multer for file uploads
+
+  // Insert the data into the "expenditure" table
+  const sql =
+    "INSERT INTO expenditure (description, cost, image_name) VALUES (?, ?, ?)";
+  const values = [description, cost, imageName];
+
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      console.error("Error inserting expenditure data into the database:", err);
+      res
+        .status(500)
+        .json({ error: "An error occurred while adding the expenditure." });
+    } else {
+      console.log("Expenditure data inserted into the database.");
+      res.status(200).json({ message: "Expenditure added successfully." });
     }
   });
 });
